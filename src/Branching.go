ea6ba36130678a87fc2dd2284a3f203fc8e696b7
Jalpesh Vadgama <jalpesh@dotnetjalps.com>
2015-10-10 16:06:57 +0530

    Added branching go file.

---
+++ src/Branching.go
@@ -0,0 +1,20 @@
+package main
+import (
+	"fmt"
+)
+func main()  {
+	foo := 1
+	if foo==1{
+		fmt.Println("value is 1")
+	}
+
+	if another:=2;another==2{
+		fmt.Println("value is 2")
+	}
+	switch foo {
+	case 1:
+		fmt.Println("1")
+	case 2:
+		fmt.Println("2")
+	}
+}
