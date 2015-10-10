cfe15591304c9d8601dd6ffd23c77f1443038101
Jalpesh Vadgama <jalpesh@dotnetjalps.com>
2015-10-10 15:13:58 +0530

    Added premitive data type file

---
+++ src/PremetiveDataTypes.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main()  {
+	var number int
+	number =43
+	fmt.Println(number)
+
+	var floatNumber float32
+	floatNumber = 45.50
+	fmt.Println(floatNumber)
+
+	myString := "Hello world"
+	fmt.Println(myString)
+
+
+}
