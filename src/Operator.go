7bc57565aadbafdb7aebd0d642d9c766c69aa54e
Jalpesh Vadgama <jalpesh@dotnetjalps.com>
2015-10-10 15:47:21 +0530

    Added operator file in go.

---
+++ src/Operator.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main()  {
+	add:= 1+ 2
+	fmt.Println(add)
+
+	subtract := 15-20
+	fmt.Println(subtract)
+
+	inc :=0
+	inc++
+	fmt.Println(inc)
+	inc++
+	fmt.Println(inc)
+
+}
